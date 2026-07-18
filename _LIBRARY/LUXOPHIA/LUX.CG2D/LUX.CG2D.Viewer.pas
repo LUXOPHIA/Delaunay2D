f6f41c4c339f438fafb6962b6a44c8c5acf95c21
luxidea <contact@luxidea.net>
2026-07-19 03:21:10 +0900

    TCGViewer クラスから、Layers 依存を除去。

--- _LIBRARY/LUXOPHIA/LUX.CG2D/LUX.CG2D.Viewer.pas
+++ _LIBRARY/LUXOPHIA/LUX.CG2D/LUX.CG2D.Viewer.pas
@@ -2,26 +2,23 @@
 
 // シーングラフの汎用ビューア（カメラ越しにシーンを描く）
 //
-// ・Camera プロパティに TCGCamera を接続すると、カメラの属すシーン（TCGLayers）を
+// ・Camera プロパティに TCGCamera を接続すると、カメラの属すシーンを
 //   カメラから見た風景として描画する。Camera が nil ならべた塗りのみ。
-//   カメラはシーンに所属させてから接続すること（所属先からシーンを解決する）。
-// ・シーンの OnChange（TDelegates による多播）を購読し、変化のたびに自動で
-//   再描画する。外から Repaint を呼ぶ必要はなく、他の購読者とも共存できる。
+// ・シーンの変化はカメラの OnScene（TDelegates による多播）で購読し、変化のたびに
+//   自動で再描画する。外から Repaint を呼ぶ必要はなく、他の購読者とも共存できる。
 // ・カメラの視野（SizeX / SizeY）が UI にぴったり完全に収まる等方スケールで描く。
 //   UI と視野のアスペクト比が異なる場合、視野は長い辺の方向へ広がる
 //   （視野 -1〜+1 × -1〜+1 を 3:2 の UI に映すと -1.5〜+1.5 × -1〜+1 になる）。
 // ・Paint をオーバーライドして Skia キャンバスへ直接レンダリングする。
 //   GlobalUseSkia = True が前提。
-// ・シーンを先に解放する場合は Camera := nil で購読を外してから解放すること。
+// ・カメラを先に解放する場合は Camera := nil で購読を外してから解放すること。
 
 interface //#################################################################### ■
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Math,
-  System.Math.Vectors, System.Skia,
+  System.Skia,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Skia, FMX.Skia.Canvas,
-  LUX.D3x3,
-  LUX.Data.Tree,
   LUX.CG2D;
 
 type
@@ -29,11 +26,10 @@ type
   private
   protected
     _Camera :TCGCamera;
-    _Layers :TCGLayers;  // カメラから解決したシーン（購読の管理用）
     ///// A C C E S S O R
     procedure SetCamera( const Camera_:TCGCamera );
     ///// M E T H O D
-    procedure LayersChange( Sender_:TObject );
+    procedure SceneChange( Sender_:TObject );
     procedure Paint; override;
   public
     destructor Destroy; override;
@@ -50,29 +46,19 @@ implementation //###############################################################
 //////////////////////////////////////////////////////////////// A C C E S S O R
 
 procedure TCGViewer.SetCamera( const Camera_:TCGCamera );
-var
-   R :TTreeNode;
 begin
-     if Assigned( _Layers ) then _Layers.OnChange.Del( LayersChange );
+     if Assigned( _Camera ) then _Camera.OnScene.Del( SceneChange );
 
      _Camera := Camera_;
-     _Layers := nil;
 
-     if Assigned( _Camera ) then
-     begin
-          R := _Camera.Root;  // カメラの所属からシーンを解決する
-
-          if R is TCGLayers then _Layers := TCGLayers( R );
-     end;
-
-     if Assigned( _Layers ) then _Layers.OnChange.Add( LayersChange );
+     if Assigned( _Camera ) then _Camera.OnScene.Add( SceneChange );
 
      Repaint;
 end;
 
 //////////////////////////////////////////////////////////////////// M E T H O D
 
-procedure TCGViewer.LayersChange( Sender_:TObject );
+procedure TCGViewer.SceneChange( Sender_:TObject );
 begin
      Repaint;
 end;
@@ -83,7 +69,6 @@ procedure TCGViewer.Paint;
 var
    Canvas_ :ISkCanvas;
    S :Single;
-   V :TSingleM3;
 begin
      if not ( Canvas is TSkCanvasCustom ) then Exit;  // 要 GlobalUseSkia = True
 
@@ -93,7 +78,7 @@ begin
      try
           Canvas_.ClipRect( TRectF.Create( 0, 0, Width, Height ) );
 
-          if Assigned( _Layers ) then
+          if Assigned( _Camera ) then
           begin
                Canvas_.Translate( Width / 2, Height / 2 );  // 視野の中心 → 画面中央
 
@@ -101,13 +86,9 @@ begin
 
                Canvas_.Scale( S, S );
 
-               V := _Camera.GlobalPose.Inverse;
-
-               Canvas_.Concat( TMatrix( V ) );  // ワールド座標系 → カメラ座標系
-
-               _Layers.Render( Canvas_ );
+               _Camera.Render( Canvas_ );  // カメラ座標系への変換とシーンの描画
           end
-          else Canvas_.Clear( TAlphaColors.White );  // カメラ（シーン）が無ければべた塗りのみ
+          else Canvas_.Clear( TAlphaColors.White );  // カメラが無ければべた塗りのみ
      finally
           Canvas_.Restore;
      end;
@@ -117,7 +98,7 @@ end;
 
 destructor TCGViewer.Destroy;
 begin
-     if Assigned( _Layers ) then _Layers.OnChange.Del( LayersChange );  // 購読解除
+     if Assigned( _Camera ) then _Camera.OnScene.Del( SceneChange );  // 購読解除
 
      inherited;
 end;
